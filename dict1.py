11da8adcc0c00e8e7602b3a1cf1bd6fbd57d8448
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-24 19:09:36 +0530

    dictio

---
+++ dict1.py
@@ -0,0 +1,12 @@
+vm = {
+    "name": "prod-web-01",
+    "location": "centralindia",
+    "status": "Running",
+    "public_ip": True,
+    "port": 22
+}
+print(f"VM Name: {vm['name']}")
+print(f"VM Location: {vm['location']}")
+print(f"VM Status: {vm['status']}")
+print(f"VM Public IP: {vm['public_ip']}")
+print(f"VM Port: {vm['port']}")
