b7096931863d3791f84fcd869ebf4e291a9aeef7
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-25 19:36:42 +0530

    commit 2

--- azureapi5.py
+++ azureapi5.py
@@ -13,4 +13,27 @@ for subscription in client.subscriptions.list():
         subscription_id = subscription.subscription_id
         break
 
-print("Subscription:", subscription_id)
+token = credential.get_token(
+    "https://management.azure.com/.default"
+)
+
+headers = {
+    "Authorization": f"Bearer {token.token}"
+}
+
+url = (
+    f"https://management.azure.com/"
+    f"subscriptions/{subscription_id}/resources"
+    f"?api-version=2021-04-01"
+)
+
+response = requests.get(
+    url,
+    headers=headers
+)
+
+# print("Status:", response.status_code)
+
+data = response.json()
+
+# print(type(data))
