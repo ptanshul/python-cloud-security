fbda0be8de19f054714496d3b4a8caf302c3a7e4
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-25 13:08:39 +0530

    commit1

---
+++ dict5.py
@@ -0,0 +1,23 @@
+assignments = [
+    {
+        "properties": {
+            "principalId": "111",
+            "role": "Reader",
+            "environment": "production"
+        }
+    },
+    {
+        "properties": {
+            "principalId": "222",
+            "role": "Owner",
+            "environment": "production"
+        }
+    },
+    {
+        "properties": {
+            "principalId": "333",
+            "role": "Owner",
+            "environment": "development"
+        }
+    }
+]
