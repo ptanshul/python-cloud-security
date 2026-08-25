98c8fcef7a73924f52dea7c8dbc03fdeb5a1a8dc
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-25 19:23:10 +0530

    commit2

--- azureapi4.py
+++ azureapi4.py
@@ -1,12 +1,12 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/todos/1"
+url = "https://jsonplaceholder.typicode.com/todos"
 
 response = requests.get(url)
 
 data = response.json()
 
 print(data)
-print("User ID:", data['userId'])
-print("Title:", data['title'])
-print ("Completed:", data['completed'])
+print("User ID:", data[0]['userId'])
+print("Title:", data[0]['title'])
+print("Completed:", data[0]['completed'])
