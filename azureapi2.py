32412286c94cdd9bdef18c68e9ed708d902e4b06
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-25 17:33:54 +0530

    commit1

---
+++ azureapi2.py
@@ -0,0 +1,10 @@
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import SubscriptionClient
+
+credential = AzureCliCredential()
+
+client = SubscriptionClient(credential)
+
+for subscription in client.subscriptions.list():
+    print("Subscription ID:", subscription.subscription_id)
+    print("Subscription Name:", subscription.display_name)
