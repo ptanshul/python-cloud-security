7f3cae8968dd336bc94a8bd2b32b143f50f035cc
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-25 19:16:29 +0530

    commit1

---
+++ azureapi3.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/todos/1"
+
+response = requests.get(url)
+
+data = response.json()
+
+print(data)
+
+print("Title:", data['title'])
+print ("Completed:", data['completed'])
